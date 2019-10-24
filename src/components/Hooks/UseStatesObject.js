cde90aebf7745c3a04a1b60ea32b4bda152a4d24
Moch Agung Hermawan <39933034+mochagungh@users.noreply.github.com>
2019-10-24 10:38:38 +0700

    gabut

---
+++ src/components/Hooks/UseStatesObject.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+function UseStateObj() {
+	const [name, setName] = useState({ firstname: '', lastname: '' });
+	return (
+		<>
+			<input
+				type="text"
+				value={name.firstname}
+				onChange={e => setName({ ...name, firstname: e.target.value })}
+			/>
+			<input
+				type="text"
+				value={name.lastname}
+				onChange={e => setName({ ...name, lastname: e.target.value })}
+			/>
+			<h2>Your firstname is {name.firstname}</h2>
+			<h2>Your lastname is {name.lastname}</h2>
+		</>
+	);
+}
+
+export default UseStateObj;
--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ import SisaHidup from './components/SisaHidup/index';
 import './styles.css';
 // import Game from './components/ParticleOfComponentOfCompponents/TicTacToe';
 import Todos from './components/Todos/HookHooks';
+import UseStateObj from './components/Hooks/UseStatesObject';
 // import MessageList from './components/ParticleOfComponentOfCompponents/Context';
 
 // const rootElement = document.getElementById('root');
 // ReactDOM.render(<CountDown hours="0" minutes="1" />, rootElement);
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<SisaHidup />, rootElement);
+ReactDOM.render(<UseStateObj />, rootElement);
