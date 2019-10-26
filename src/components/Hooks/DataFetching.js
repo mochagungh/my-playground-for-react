87e8db9af9bc551fa6d78a843531f172d053e632
Moch Agung Hermawan <39933034+mochagungh@users.noreply.github.com>
2019-10-26 20:33:53 +0700

    another gbt

--- src/components/Hooks/DataFetching.js
+++ src/components/Hooks/DataFetching.js
@@ -2,29 +2,40 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function DataFetching() {
-	const [posts, setPosts] = useState([]);
+	const [posts, setPosts] = useState({});
+	const [postId, setPostId] = useState(1);
 
 	useEffect(() => {
 		axios
-			.get('https://jsonplaceholder.typicode.com/posts')
+			.get(`https://jsonplaceholder.typicode.com/posts/${postId}`)
 			.then(res => {
+				console.log(res);
 				setPosts(res.data);
 			})
 			.catch(err => {
 				console.log(err);
 			});
-	}, []);
+	}, [postId]);
 
 	return (
-		<ul>
-			{posts.map(post => (
-				<li key={post.id}>
-					{post.id}
-					<br />
-					{post.title}
-				</li>
-			))}
-		</ul>
+		<>
+			<input
+				type="text"
+				value={postId}
+				onChange={e => setPostId(e.target.value)}
+			/>
+			<li>{posts.title}</li>
+		</>
+
+		// <ul>
+		// 	{posts.map(post => (
+		// 		<li key={post.id}>
+		// 			{post.id}
+		// 			<br />
+		// 			{post.title}
+		// 		</li>
+		// 	))}
+		// </ul>
 	);
 }
 
