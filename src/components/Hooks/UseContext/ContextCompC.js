ce6c633c8d896d4d191df193320913dab00381ab
Moch Agung Hermawan <39933034+mochagungh@users.noreply.github.com>
2019-10-29 08:40:22 +0700

    useContext

--- src/components/Hooks/UseContext/ContextCompC.js
+++ src/components/Hooks/UseContext/ContextCompC.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { UserContext, ChannelContext } from '../../../App';
 
 function ContextCompC() {
+	// USECONTEXT
+	const channelColor = useContext(ChannelContext);
+	const color = useContext(UserContext);
+
 	return (
 		<>
+			{/* ############ OLD WAY CONTEXT API ############## */}
 			<UserContext.Consumer>
 				{color => {
 					return (
 						<ChannelContext.Consumer>
 							{channel => {
 								return (
-									<div>
+									<p>
 										user context value color is <strong>{color}</strong> <br />{' '}
 										and <br />
-										channel context value color is <strong>{channel}</strong>
-									</div>
+										channel context value color is{' '}
+										<strong>{channelColor}</strong>
+									</p>
 								);
 							}}
 						</ChannelContext.Consumer>
 					);
 				}}
 			</UserContext.Consumer>
+
+			{/* ##########  USECONTEXT API ########## */}
+			<p>
+				user context value color is <strong>{color}</strong> <br /> and <br />
+				channel context value color is <strong>{channelColor}</strong>
+			</p>
 		</>
 	);
 }
